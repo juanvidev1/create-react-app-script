5081868030c439159526fd13d592a46c273edaa1
juanvidev1 <juanvidev1@gmail.com>
2025-01-07 12:34:43 -0500

    Initial commit

---
+++ create-react-app.sh
@@ -0,0 +1,126 @@
+#! /bin/bash
+# Program to create and configure locally a new react app with the Soundlutions-Commit template
+# Author: Juanvidev <juanvidev1@gmail.com>
+
+initial_path=$(pwd)
+echo "Actual path: $initial_path"
+sleep 1
+option=0
+app_path=""
+app_name=""
+github_url=""
+extension=""
+apiExtension=""
+
+# Charges the nvm module so can be used inside the bash script
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Esto carga nvm
+
+echo "Welcome to create react app script"
+read -e -p "Enter the path to create your app: " app_path
+read -p "Enter the name of your app: " app_name
+read -p "Select an option: 1 for JavaScript or 2 for TypeScript: " option
+read -p "If you want to create a remote repository, enter the url (make sure you have access rigths first) " github_url
+
+# # This checks if the path exists
+# if [[ ! -d $app_path ]]; then
+#     echo "The path $app_path doesn't exists"
+#     exit 1
+# fi
+
+echo "Creating app in $app_path"
+
+
+# This creates the directory or the path if doesn't exists
+if [[ ! -d $app_path ]]; then
+    echo "Creating directory $app_path"
+    mkdir -p $app_path
+fi
+
+cd $app_path
+# echo "Este es el path $app_path"
+
+if [[ $option == 1 ]]; then
+    echo "Creating app with JavaScript"
+    apiExtension="js"
+    extension="jsx"
+    npm create vite@latest $app_name -- --template react -- --no-interactive
+elif [[ $option == 2 ]]; then
+    echo "Creating app with TypeScript"
+    apiExtension="ts"
+    extension="tsx"
+    npm create vite@latest $app_name -- --template react-ts -- --no-interactive
+else
+    echo "Invalid option"
+fi
+
+new_path=$app_path/$app_name
+echo "New path: $new_path"
+sleep 3
+cd $new_path
+echo "Entering your app directory in $new_path"
+nvm use lts/jod # Uses the jod lts version the same that node 22.12.0
+node -v > $new_path/.nvmrc
+cd $new_path/src
+mkdir components lib hooks utils
+cp $initial_path/createApiFile.sh $app_path
+cp $initial_path/createHookFile.sh $app_path
+cp $initial_path/createComponentFile.sh $app_path
+cp $initial_path/modifyTailwindConfig.sh $app_path
+cp $initial_path/modifyApp.sh $app_path
+cp $initial_path/createREADME.sh $app_path
+bash $app_path/createApiFile.sh api $new_path/src lib $apiExtension
+bash $app_path/createHookFile.sh useSoundlutionsForm $new_path/src/hooks $extension
+bash $app_path/createComponentFile.sh ExampleForm $new_path/src components $extension
+bash $app_path/modifyApp.sh $new_path/src $extension
+sleep 3
+
+cd $new_path
+echo "Installing dependencies...$new_path"
+npm i
+echo "Dependencies installed successfully"
+
+echo "Installing tailwindcss and postcss in $new_path"
+sleep 3
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+sleep 2
+bash $app_path/modifyTailwindConfig.sh $new_path
+echo "Tailwindcss settings finished"
+sleep 2
+bash $app_path/createREADME.sh $new_path
+sleep 2
+
+echo "Repo url $github_url"
+if [ -z "$github_url" ]; then
+    echo "Skipping remote repository creation"
+    git init
+    git add .
+    git commit -m "Initial commit"
+    echo "Local repository created"
+else 
+    echo "Creating remote repository"
+    git init
+    git add .
+    git commit -m "Initial commit"
+    git branch -M main
+    git remote add origin $github_url
+    git push -u origin main
+    echo "Remote repository created"
+fi
+
+echo "Path for running the app $new_path"
+npm run dev
+
+URL="http://localhost:5173"
+
+# Comprobar el sistema operativo y abrir la URL en el navegador
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    xdg-open "$URL"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open "$URL"
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    explorer.exe "$URL"
+else
+    echo "No se soporta este sistema operativo"
+fi
---
+++ createComponentFile.sh
@@ -0,0 +1,120 @@
+#! /bin/bash
+# Script to create a new file inside components
+# Author: Juanvidev <juanvidev1@gmail.com>
+
+file_name=$1
+file_path=$2
+file_folder=$3
+extension=$4
+
+if [ -z "$file_name" ]; then
+  echo "Please provide a file name"
+  exit 1
+fi
+
+if [ -z "$file_path" ]; then
+  echo "You must provide a file path"
+fi
+
+if [ -z "$file_folder" ]; then
+  file_folder="components"
+fi
+
+if [ -z "$extension" ]; then
+  extension="jsx"
+fi
+
+
+mkdir -p $file_path/$file_folder/$file_name
+touch $file_path/$file_folder/$file_name/index.$extension
+
+cat<<EOF > $file_path/$file_folder/$file_name/index.$extension
+import React from "react";
+import { useSoundlutionsForm } from "../../hooks/useSoundlutionsForm";
+
+/**
+ * Este es un componente de ejemplo que utiliza el hook useSoundlutionsForm
+ * El hook useSoundlutionsForm se encarga de manejar el estado del formulario y las validaciones, de acuerdo a lo que se ha experimentado a través
+ * de los diversos proyectos que se han trabajado en front-end.
+ * Se sugiere no modificar el hook, sino crear un nuevo hook si es necesario, por eso el template incluye una carpeta hooks para crear los custom
+ * hooks que sean necesarios.
+ * @returns formElement
+ */
+export const ExampleForm = () => {
+  const { formData, formErrors, onChange, onValidate } = useSoundlutionsForm();
+
+  /**
+   * No se tiene una función de submit en el hook, debido a que todos los submit son muy diferentes
+   * Función que maneja el envío del formulario
+   * @param {event} e
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (onValidate(e)) {
+      // Aquí puedes enviar los datos del formulario
+      console.log("Datos del formulario:", formData);
+      alert(`Datos del formulario: ${JSON.stringify(formData)}`);
+    }
+  };
+
+  /**
+   * Este useEffect se encarga de mostrar en consola los errores del formulario, por si se quiere ver el estado de los errores en el formulario.
+   * En el formulario que se vaya a implementar, se puede eliminar este useEffect y mostrar los errores en el componente que se desee, por ejemplo,
+   * en un componente de error que se muestre en el formulario.
+   */
+  React.useEffect(() => {
+    console.log("FormErrors:", formErrors);
+  }, [formErrors]);
+
+  return (
+    <form onSubmit={handleSubmit} method="POST">
+      <div>
+        <label htmlFor="name">Nombre:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={onChange}
+          className="bg-slate-400 ml-2"
+        />
+        {formErrors.name && (
+          <span className="text-xs text-red-500 ml-2">{formErrors.name}</span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={onChange}
+          className="bg-slate-400 ml-2 my-2"
+        />
+        {formErrors.email && (
+          <span className="text-xs text-red-500 ml-2">{formErrors.email}</span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="country">País:</label>
+        <select id="country" name="country" onChange={onChange}>
+          <option value="">Selecciona un país</option>
+          <option value="mx">México</option>
+          <option value="us">Estados Unidos</option>
+          <option value="ca">Canadá</option>
+        </select>
+        {formErrors.country && (
+          <span className="text-xs text-red-500 ml-2">
+            {formErrors.country}
+          </span>
+        )}
+      </div>
+      <button type="submit" className="mt-3 bg-amber-300 text-black py-2 px-3">
+        Enviar
+      </button>
+    </form>
+  );
+};
+EOF
+
+echo "File $file_name created successfully"
+
